d3780f9ed72a3c08d0d3f1c94f3f39beb3813f13
Émile Perron <contact@emileperron.com>
2021-02-12 20:59:44 -0500

    Added favicon stuff

--- parts/head.php
+++ parts/head.php
@@ -5,3 +5,14 @@
 <link rel="preconnect" href="https://fonts.gstatic.com">
 <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@800&family=Poppins:wght@300;400;500;600&display=swap" rel="stylesheet">
 <script src="https://kit.fontawesome.com/b70270e2e7.js" crossorigin="anonymous"></script>
+<link rel="apple-touch-icon" sizes="180x180" href="/assets/icons/apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="32x32" href="/assets/icons/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="/assets/icons/favicon-16x16.png">
+<link rel="manifest" href="/assets/icons/site.webmanifest">
+<link rel="mask-icon" href="/assets/icons/safari-pinned-tab.svg" color="#991747">
+<link rel="shortcut icon" href="/assets/icons/favicon.ico">
+<meta name="apple-mobile-web-app-title" content="Toitures Bellevue">
+<meta name="application-name" content="Toitures Bellevue">
+<meta name="msapplication-TileColor" content="#b91d47">
+<meta name="msapplication-config" content="/assets/icons/browserconfig.xml">
+<meta name="theme-color" content="#991747">
