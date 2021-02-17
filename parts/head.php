8d00e4d918363ea618c5265c42cea2c5410d8b19
Émile Perron <contact@emileperron.com>
2021-02-17 18:36:04 -0500

    Bumped styles version

--- parts/head.php
+++ parts/head.php
@@ -1,7 +1,7 @@
 <meta charset="UTF-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<link rel="stylesheet" href="/assets/css/style.css?v=0.0.3">
+<link rel="stylesheet" href="/assets/css/style.css?v=0.0.4">
 <link rel="preconnect" href="https://fonts.gstatic.com">
 <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@800&family=Poppins:wght@300;400;500;600&display=swap" rel="stylesheet">
 <script src="https://kit.fontawesome.com/b70270e2e7.js" crossorigin="anonymous"></script>
