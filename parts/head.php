2779d995d82732a3eabd5b8c276e6e93708e2703
Émile Perron <contact@emileperron.com>
2021-03-04 21:26:24 -0500

    Bumped CSS version

--- parts/head.php
+++ parts/head.php
@@ -4,7 +4,7 @@ setlocale(LC_ALL, 'fr_CA.UTF-8');
 <meta charset="UTF-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<link rel="stylesheet" href="/assets/css/style.css?v=0.0.4">
+<link rel="stylesheet" href="/assets/css/style.css?v=0.0.5">
 <link rel="preconnect" href="https://fonts.gstatic.com">
 <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@800&family=Poppins:wght@300;400;500;600&display=swap" rel="stylesheet">
 <link rel="stylesheet" href="/assets/fontawesome/css/all.min.css">
