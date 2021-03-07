f9a591f6c9f5344ee3a6feedfc9bebf653f50bc9
Émile Perron <contact@emileperron.com>
2021-03-06 20:29:40 -0500

    Style version bump

--- parts/head.php
+++ parts/head.php
@@ -5,7 +5,7 @@ date_default_timezone_set('America/Toronto');
 <meta charset="UTF-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<link rel="stylesheet" href="/assets/css/style.css?v=0.1.3">
+<link rel="stylesheet" href="/assets/css/style.css?v=0.1.4">
 <link rel="preconnect" href="https://fonts.gstatic.com">
 <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@800&family=Poppins:wght@300;400;500;600&display=swap" rel="stylesheet">
 <link rel="stylesheet" href="/assets/fontawesome/css/all.min.css">
