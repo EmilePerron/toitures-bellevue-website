af544e5bb78d88f51b79b386ca9a2e4e286175a8
Émile Perron <contact@emileperron.com>
2021-03-07 10:28:28 -0500

    Added 404 and 500 pages

---
+++ _errors/404.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="fr-CA">
+	<head>
+		<title>Page introuvable - Toitures Bellevue</title>
+		<?php include '../parts/head.php' ?>
+	</head>
+	<body>
+		<?php include '../parts/header.php' ?>
+
+		<main>
+			<div id="page-header">
+				<div class="container">
+					<h1>Erreur 404: page introuvable</h1>
+				</div>
+			</div>
+			
+			<section>
+				<div class="container">
+					<p>La page à laquelle vous tentez d'accéder ne semble pas exister.</p>
+					
+					<div class="button-container">
+						<a href="/" class="button">Retourner à l'accueil du site</a>
+					</div>
+				</div>
+			</section>
+
+		<?php include '../parts/footer.php' ?>
+	</body>
+</html>
---
+++ _errors/500.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="fr-CA">
+	<head>
+		<title>Erreur - Toitures Bellevue</title>
+		<?php include '../parts/head.php' ?>
+	</head>
+	<body>
+		<?php include '../parts/header.php' ?>
+
+		<main>
+			<div id="page-header">
+				<div class="container">
+					<h1>Une erreur s'est produite</h1>
+				</div>
+			</div>
+			
+			<section>
+				<div class="container">
+					<p>Une erreur s'est produite lors du chargement de cette page. Veuillez vérifier le lien auquel vous tentez d'accéder ou revenir plus tard.</p>
+					<p>Nous sommes désolés pour cette inconvénient.</p>
+					
+					<div class="button-container">
+						<a href="/" class="button">Retourner à l'accueil du site</a>
+					</div>
+				</div>
+			</section>
+
+		<?php include '../parts/footer.php' ?>
+	</body>
+</html>
